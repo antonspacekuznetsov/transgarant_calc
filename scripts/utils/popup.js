775fc787f7a4beaf14fac89ff12369bd7c65ff69
Kuznetsov Anton <as.ky@ya.ru>
2020-05-06 15:43:53 +0300

    added popup functions for category block

--- scripts/utils/popup.js
+++ scripts/utils/popup.js
@@ -6,7 +6,8 @@ define(["ko", 'text!/templates/popup_template.html'], function(ko, template){
             var tooltipElem = document.getElementById('category_info' + index);
 
             var left = coords.left +20;
-            if (left < 0) left = 0; // не вылезать за левую границу окна
+            if(document.body.offsetWidth - left < 500) left = coords.left - 500;
+            if (left < 0) left = 20; // не вылезать за левую границу окна
       
             var top = coords.top - tooltipElem.offsetHeight + 30;
             if (top < 0) { // не вылезать за верхнюю границу окна
@@ -15,12 +16,13 @@ define(["ko", 'text!/templates/popup_template.html'], function(ko, template){
 
             tooltipElem.style.left = left + 'px';
             tooltipElem.style.top = top + 'px';
-
+            tooltipElem.style.display = '';
            
         };
 
-        this.hidePopup = function(){
-
+        this.hidePopup = function(data, event, index){
+            var tooltipElem = document.getElementById('category_info' + index);
+            tooltipElem.style.display = 'none';
         }
     }
 
